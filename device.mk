11d2355163945084bb40b506dc2c77d721023336
Michael Bestas <mkbestas@gmail.com>
2024-11-18 12:52:46 +0545

    Fixup mojito: Shim widevine with libcrypto_shim The following change google/boringssl@e202e51 removed symbols that our libs require.
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: romgharti <romgharti@gmail.com>
    Signed-off-by: build-13 <173170056+build-13@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -225,14 +225,9 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3 \
-    android.hardware.drm@1.3.vendor
-    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey \
-    libcrypto_shim \
     libcrypto_shim.vendor
 
 # fastbootd
--- extract-files.sh
+++ extract-files.sh
@@ -72,11 +72,9 @@ function blob_fixup() {
             patchelf --remove-needed "libmegface.so" "${2}"
             patchelf --add-needed "libshim_megvii.so" "${2}"
             ;;
-        vendor/lib64/libwvhidl.so)
-            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
-            ;;
-        vendor/lib64/mediadrm/libwvdrmengine.so)
-            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+        vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
     esac
 }
