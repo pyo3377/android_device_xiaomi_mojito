69598f4aba76a546260bd4a84dab34fe536c2e34
Michael Bestas <mkbestas@gmail.com>
2024-11-18 12:52:46 +0545

    mojito: Shim widevine with libcrypto_shim [1/2] The following change google/boringssl@e202e51 removed symbols that our libs require.
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: build-13 <173170056+build-13@users.noreply.github.com>
    
    mojito: Ensure we build the vendor version of libcrypto_shim
    Fix:
    F linker  : CANNOT LINK EXECUTABLE /vendor/bin/hw/android.hardware.drm@1.3-service.widevine: library libcrypto_shim.so not found: needed by /vendor/lib64/libwvhidl.so in namespace (default)
    
    Reference: Ia42de59ff157cddc930a111f8ce5e36e645055ed
    Change-Id: Ie9823e19a86e3066343c010f34f7832b167a2633
    Signed-off-by: build-13 <173170056+build-13@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -228,7 +228,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3 \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim \
+    libcrypto_shim.vendor
 
 # fastbootd
 PRODUCT_PACKAGES += \
